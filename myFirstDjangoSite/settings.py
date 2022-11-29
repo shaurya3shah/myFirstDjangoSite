d0b57ee9507f8cb14e093db77c1a7fc5dbae2663
nakulshah <nakulshah@gmail.com>
2022-11-29 16:50:59 -0600

    committing newest

--- myFirstDjangoSite/settings.py
+++ myFirstDjangoSite/settings.py
@@ -75,10 +75,10 @@ WSGI_APPLICATION = 'myFirstDjangoSite.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
