76a9a6d783e0a1dd667206955db211506b027b22
nakulshah <nakulshah@gmail.com>
2022-11-29 22:36:09 -0600

    very close

--- helloapp/urls.py
+++ helloapp/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'helloapp'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('check_guess', views.check_guess, name='check_guess')
 ]
--- helloapp/views.py
+++ helloapp/views.py
@@ -1,3 +1,4 @@
+import random
 from typing import Set
 
 from django.shortcuts import render
@@ -8,13 +9,35 @@ from django.template import loader
 
 from helloapp.models import GuessedNumber
 
+secret_number: int = 0
+check_count: int = 0
+
 
 def index(request):
     template = loader.get_template('helloapp/index.html')
-    guessed_number = GuessedNumber
-    guessed_number.guessed_number = 10
-    guessed_number.comparison = 'greater'
+    global secret_number
+    secret_number = random.randint(1, 100)
 
-    context = {'gnumber': guessed_number}
+    context = {'secret_number': secret_number}
+
+    return HttpResponse(template.render(context, request))
+
+
+def check_guess(request):
+    template = loader.get_template('helloapp/index.html')
+    number = int(request.POST["Enter Your Guess"])
+    user_guess = GuessedNumber
+    user_guess.guessed_number = number
+    global secret_number
+    global check_count
+    if user_guess.guessed_number < secret_number:
+        user_guess.comparison = 'lesser'
+    elif user_guess.guessed_number > secret_number:
+        user_guess.comparison = 'greater'
+    else:
+        user_guess.comparison = 'equal'
+
+    check_count += 1
+    context = {'user_guess': user_guess, 'guess_count': check_count}
 
     return HttpResponse(template.render(context, request))
