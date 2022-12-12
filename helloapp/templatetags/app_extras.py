8cb999ff331563f262e1ac26253afcb7e14faa73
nakulshah <nakulshah@gmail.com>
2022-12-11 22:22:53 -0600

    more UX improvements

--- helloapp/templatetags/app_extras.py
+++ helloapp/templatetags/app_extras.py
@@ -7,3 +7,11 @@ register = template.Library()
 def blue(value, arg):
     """Returns string in HTML blue"""
     return value.replace(arg, '<span style="color: blue">' + arg + '</span>')
+
+@register.filter
+def tile(value):
+    """Returns what class of tile needs to be rendered for numberdle"""
+    if '=' in value:
+        return 'tile--correct'
+    else:
+        return 'tile'
