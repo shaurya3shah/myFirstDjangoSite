437f8a47d4d5c1ac70bbe34d131e9de29c26e524
nakulshah <nakulshah@gmail.com>
2022-12-19 21:54:40 -0600

    now / will be redirected to my fun hub

--- myFirstDjangoSite/urls.py
+++ myFirstDjangoSite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from helloapp import views
 
 urlpatterns = [
     path('helloapp/', include('helloapp.urls')),
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
