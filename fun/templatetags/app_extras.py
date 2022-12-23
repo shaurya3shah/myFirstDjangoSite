0d1975918685388e628cbb1ba6d60e2aa1a4ca7e
nakulshah <nakulshah@gmail.com>
2022-12-23 06:48:04 -0600

    hints in numberdle

--- fun/templatetags/app_extras.py
+++ fun/templatetags/app_extras.py
@@ -8,6 +8,7 @@ def blue(value, arg):
     """Returns string in HTML blue"""
     return value.replace(arg, '<span style="color: blue">' + arg + '</span>')
 
+
 @register.filter
 def tile(value):
     """Returns what class of tile needs to be rendered for numberdle"""
@@ -15,3 +16,16 @@ def tile(value):
         return 'tile--correct'
     else:
         return 'tile--incorrect'
+
+
+@register.filter
+def hint(value):
+    """Returns what hint needs to be provided for numberdle"""
+    if '=' in value:
+        return ' class="hint--success" aria-label="Correct!"'
+    elif '>' in value:
+        return ' class="hint--info" aria-label="Input Number > Hidden Number"'
+    elif '<' in value:
+        return ' class="hint--info" aria-label="Input Number < Hidden Number"'
+    else:
+        return ' '
