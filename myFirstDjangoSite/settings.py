060e1da970f3e499db6522281f7ff16e76a30a17
nakulshah <nakulshah@gmail.com>
2023-02-27 22:02:07 -0600

    display username after login also, logout

--- myFirstDjangoSite/settings.py
+++ myFirstDjangoSite/settings.py
@@ -134,6 +134,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 
 # Initialise environment variables
 env = environ.Env()
